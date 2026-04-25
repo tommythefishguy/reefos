7109e1d374571fdcc651bcd3f032db5f5fdb2700
tommythefishguy <maloneytommy83@gmail.com>
2026-04-25 23:20:12 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const C='reefos-github-stable-v2';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
