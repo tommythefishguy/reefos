902503d8cdd4f80efdef74bf43328b361c337a0c
tommythefishguy <maloneytommy83@gmail.com>
2026-04-25 23:34:25 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const C='reefos-custom-params-themes-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(C).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',event=>{
+  event.respondWith(fetch(event.request).then(res=>{
+    const copy=res.clone();
+    caches.open(C).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+    return res;
+  }).catch(()=>caches.match(event.request)));
+});
