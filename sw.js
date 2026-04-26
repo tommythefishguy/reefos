c633037dc3bddccd7af082504094fd76b11f40bc
tommythefishguy <maloneytommy83@gmail.com>
2026-04-26 18:21:59 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const C='aquorax-rebrand-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(res=>{let copy=res.clone();caches.open(C).then(c=>c.put(e.request,copy)).catch(()=>{});return res}).catch(()=>caches.match(e.request)))});
